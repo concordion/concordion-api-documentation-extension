6132ae22ece7ea5454c4ce29924591f54331be8a
Jens Neuhalfen <jens@neuhalfen.name>
2017-08-10 17:03:51 +0200

    Add test for unimplemented languages

---
+++ src/test/groovy/spec/concordion/command/executablespec/UnknownLanguagesTest.groovy
@@ -0,0 +1,14 @@
+package spec.concordion.command.executablespec
+
+import name.neuhalfen.concordion.extension.executablespec.ExecutableSpecExtension
+import org.concordion.api.ExpectedToFail
+import org.concordion.api.extension.Extensions
+import org.concordion.integration.junit4.ConcordionRunner
+import org.junit.runner.RunWith
+
+@RunWith(ConcordionRunner.class)
+@Extensions(ExecutableSpecExtension)
+class UnknownLanguagesTest {
+
+
+}
